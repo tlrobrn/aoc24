99f47b0de72bf3e92386695202bb4b35fdea6d72
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2024-12-02 23:25:03 -0700

    day3 part2

--- spec/day3_spec.lua
+++ spec/day3_spec.lua
@@ -1,11 +1,19 @@
 local day = require("src.day3")
 
-local TEST_INPUT = {
-  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
-}
-
 describe("part1", function()
   it("handles the test input", function()
-    assert.are.equal(161, day.part1(TEST_INPUT))
+    local input = {
+      "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+    }
+    assert.are.equal(161, day.part1(input))
+  end)
+end)
+
+describe("part2", function()
+  it("handles the test input", function()
+    local input = {
+      "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+    }
+    assert.are.equal(48, day.part2(input))
   end)
 end)
--- src/day3.lua
+++ src/day3.lua
@@ -13,7 +13,25 @@ function M.part1(input)
 end
 
 function M.part2(input)
-  return "not implemented"
+  local remainingInput = table.concat(input, " ")
+  local enabled = {}
+
+  while string.find(remainingInput, "mul%(%d%d?%d?,%d%d?%d?%)") do
+    local endOfSegment, oneBeforeStartOfNextSegment = string.find(remainingInput, "don't()", 1, true)
+    endOfSegment = endOfSegment or #remainingInput + 1
+    oneBeforeStartOfNextSegment = oneBeforeStartOfNextSegment or #remainingInput
+
+    enabled[#enabled + 1] = string.sub(remainingInput, 1, endOfSegment - 1)
+    remainingInput = string.sub(remainingInput, oneBeforeStartOfNextSegment + 1)
+
+    endOfSegment, oneBeforeStartOfNextSegment = string.find(remainingInput, "do()", 1, true)
+    endOfSegment = endOfSegment or #remainingInput + 1
+    oneBeforeStartOfNextSegment = oneBeforeStartOfNextSegment or #remainingInput
+
+    remainingInput = string.sub(remainingInput, oneBeforeStartOfNextSegment + 1)
+  end
+
+  return M.part1(enabled)
 end
 
 return M
